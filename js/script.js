d283323117f8ed49925584919b4ff20329408b89
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2026-01-11 00:37:58 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+/* --- CAROUSEL SCRIPT --- */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    
+    // Check if slides exist (only run on home page)
+    if (slides.length > 0) {
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
+}
